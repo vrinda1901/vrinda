0d09ec68056e7c445a380556aff919fadcd64ae5
vrinda1901 <vrindagoel19@gmail.com>
2024-12-28 14:28:18 +0530

    Create abcd.c

---
+++ abcd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{ char a[100];
+  clrscr();
+  printf("enter value of a:\n");
+  scanf("%s" ,a);
+  printf("%s",a);
+  printf("\n Regards vrinda");
+  getch();
+}
+  
